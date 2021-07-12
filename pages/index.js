ad64a646bb61a3a98761120df9b5d8226f24f0b7
Poline Lottin <lottinp@gmail.com>
2021-07-12 15:10:48 -0300

    refactor: Separate components

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid>
+      <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+        <Box>
+          <img src="https://github.com/polinelottin.png"></img>
+        </Box>
+      </div>
+      <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+        <Box>
+          Bem vindo
+        </Box>
+      </div>
+      <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <Box>
+          Comunidades
+        </Box>
+      </div>
+    </MainGrid>
+  )
 }
